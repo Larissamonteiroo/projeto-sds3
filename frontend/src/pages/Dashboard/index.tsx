74cd262a50813a53430d1a9441c411ca48c8f342
Larissa Monteiro <larissa.marques.1999@hotmail.com>
2021-09-10 13:17:48 -0300

    Routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,35 @@
+import BarChart from "components/barChart";
+import DataTable from "components/dataTable";
+import DunutChart from "components/donutChart";
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+
+const Dashboard = () => {
+    return (
+        <>
+        <NavBar />
+        <div className="container">
+          <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+  
+          <div className="row px-3">
+            <div className="col-sm-6">
+              <h5 className="text-center text-secundary">Taxa de Sucesso</h5>
+              <BarChart />
+            </div>
+            <div className="col-sm-6">
+              <h5>Todas Vendas</h5>
+  
+              <DunutChart />
+            </div>
+          </div>
+          <div className="py-3">
+            <h2 className="text-primary">Todas Vendas</h2>
+          </div>
+          <DataTable />
+        </div>
+        <Footer />
+      </>
+    );
+}
+
+export default Dashboard;
